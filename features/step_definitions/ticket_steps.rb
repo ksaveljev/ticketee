baf1ce574eb49d63b686ff64bb436a3029f407ed
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2011-06-14 17:12:18 +0300

    Added remove tag functionality

--- features/step_definitions/ticket_steps.rb
+++ features/step_definitions/ticket_steps.rb
@@ -1,5 +1,8 @@
 Given /^"([^\"]*)" has created a ticket for this project:$/ do |email, table|
   table.hashes.each do |attributes|
-    @project.tickets.create!( attributes.merge!( :user => User.find_by_email( email ) ) )
+    tags = attributes.delete("tags")
+    attributes.merge!(:user => User.find_by_email(email))
+    ticket = @project.tickets.create!(attributes)
+    ticket.tag!(tags) if tags
   end
 end
